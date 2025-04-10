ca320a23f7bbc468394d2c764dfe23fd279250d2
Miskachev Dima <miskachevds@yandex.ru>
2025-04-10 16:56:35 +0300

    summa-

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa6";
 
-function Order({item}){
+function Order({item, onDelete}){
     return(
         <div className="item">
             <img src={"./img/" + item.img} alt={item.title}/>
             <h2>{item.title}</h2>
             <b>{item.price}руб.</b>
-            <FaTrash className="delete-icon" />
+            <FaTrash className="delete-icon" onClick={()=> onDelete(item.id)}/>
         </div>
     )
 }
